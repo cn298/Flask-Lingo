58debbdd3dd55a42aea088881aadc2b54320b68f
cn298 <charlottenaylor@hotmail.co.uk>
2016-05-05 16:21:41 +0100

    Added files via upload
    Added files

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, g, request, url_for
+import sqlite3
 
 app = Flask(__name__)
+app.database = "blog_posts.db"
 app.secret_key = 'super spooky secret key'
 
 # A list of words and their corresponding translations for our app
-words = {"bonjour": "hello", "jambon": "ham", "fromage": "cheese"}
+# words = {"Bonjour": "hello", "Jambon": "ham", "Fromage": "cheese"}
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
-@app.route('/quiz', methods=['GET', 'POST'])
-def quiz():
-    # Reset visitor score to 0 each time page is loaded
+@app.route('/france', methods=['GET', 'POST'])
+def france():
+    g.db = connect_db()
+    cur = g.db.execute("select * from words where language= ?", ("French",))
+    words = [dict(word=row[0], translation=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    print(words)
     points = 0
     if request.method == "POST":
-        # Loop through every word + corresponding translation in dictionary
         for word, translation in words.items():
-            # Check if the users translation is correct
             if request.form[word] == translation:
-                # If it is then increase the point tally
                 points += 1
-        # Flash a message to the screen showing the users score
         flash(str(points))
-        # Then refresh the page to dispay the score
-        return redirect(url_for('quiz'))
+        return redirect(url_for('france'))
+    return render_template("france.html", words=words)
 
-    # If the request method isn't a POST request, just load the template
-    return render_template("quiz.html", words=words)
+def connect_db():
+    return sqlite3.connect(app.database)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
